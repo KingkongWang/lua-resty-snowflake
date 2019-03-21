7ce1ced2941368408ec216498580c3644d42ef32
Kingking Wang <wang19850919@gmail.com>
2019-03-21 15:19:49 +0800

    modified:   lib/resty/snowflake.lua

--- lib/resty/snowflake.lua
+++ lib/resty/snowflake.lua
@@ -18,7 +18,7 @@ ffi.cdef[[
     bool snowflake_next_id(snowflake_t*,int64_t*);
 ]]
 
-local _M = {_VERSION = '0.01'}
+local _M = {_VERSION = '0.0.1'}
 local mt = { __index = _M }
 
 function _M.new(worker_id, datacenter_id)
